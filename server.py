c2a0296aca01013860d119a4a47690ecf36e7877
Bustamor <randall.bustamante@gmail.com>
2022-09-30 08:23:10 -0700

    add to server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from turtle import bgcolor, color
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')``
 def index():
     return render_template('index.html')
 
